62784dfedab4b2bdedd4df0bd960f527534f6c07
SungJungHo1git config --global user.name SungJungHo1 <s214q@naver.com>
2023-01-23 18:29:13 +0900

    sd

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
     // ],
     unoptimized: true,
   },
+  experimental: {
+    images: {
+        unoptimized: true
+    }
+}
   // api_key를 마스킹 하고 싶을 때 아래 rewrites 예제 코드 주석 해제
   // Uncomment the rewrites example code below when you want to mask the api_key
 
