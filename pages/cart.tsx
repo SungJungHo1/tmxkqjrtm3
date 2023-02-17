51afbd3979be72b401041fcc1ebbe5ce14495a9e
SungJungHo1git config --global user.name SungJungHo1 <s214q@naver.com>
2023-02-17 22:06:27 +0900

    sd

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -60,8 +60,8 @@ const Cart: NextPage = () => {
   const find_U_D = async ()=>{
     let data
     await axios
-      // .get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${encodeURIComponent(UserId)}`, {//http://127.0.0.1/service
-      .get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${encodeURIComponent('Ua80cd1a19a12cb88657950e300a68594')}`, {//
+      .get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${encodeURIComponent(UserId)}`, {//http://127.0.0.1/service
+      // .get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${encodeURIComponent('Ua80cd1a19a12cb88657950e300a68594')}`, {//
       // .get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${'Ua80cd1a19a12cb88657950e300a68594'}`, {
       }).then((res) => {
         setCoupon_List(res.data.coupon_List)
