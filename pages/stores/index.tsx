852df64e0e48107f964df42a81ebe7ad53198b6a
SungJungHo1 <s214q@naver.com>
2023-04-11 23:44:02 +0900

    sdsd2

--- pages/stores/index.tsx
+++ pages/stores/index.tsx
@@ -75,7 +75,7 @@ const Stores: NextPage<{
 
   useEffect(()=>{
     if (data?.restaurants.length > 0){
-      axios.get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${encodeURIComponent('Ua80cd1a19a12cb88657950e300a68594')}`, {//
+      axios.get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${encodeURIComponent(sessionStorage.getItem('userId'))}`, {//
       // .get(`https://www.fastfood.p-e.kr/find_User_Data2?User_ID=${'Ua80cd1a19a12cb88657950e300a68594'}`, {
       }).then(async (res) => {
         
