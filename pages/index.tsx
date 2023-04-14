469e5eae38dbcf995873091140cdc27ebc84229c
SungJungHo1 <s214q@naver.com>
2023-04-14 20:54:34 +0900

    sdsd2

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,6 @@ import { useRouter } from 'next/router'
 import qs from 'qs'
 import { useEffect } from 'react'
 import type { Liff } from "@line/liff";
-import axios from 'axios'
-
-type UnPromise<T> = T extends Promise<infer X> ? X : T;
 
 const Index: NextPage<{ position: { longitude: number; latitude: number } | null; data: any; liff: Liff | null }> = ({
   position, liff
@@ -17,36 +14,6 @@ const Index: NextPage<{ position: { longitude: number; latitude: number } | null
     longitude: position?.longitude
   })
   
-  const getLiff = (Login) => {
-    if (!Login){
-      liff.login()
-    }
-    
-    liff.ready.then(async () => {
-      const liffprofile: UnPromise<ReturnType<typeof liff.getProfile>> = await liff.getProfile();
-      sessionStorage.setItem("userId", liffprofile.userId)
-      sessionStorage.setItem("userName", liffprofile.displayName)
-      // FlareLane.setUserId(liffprofile.userId);
-      // FlareLane.setTags({platform:"line",UserName:liffprofile.displayName})
-
-      liffprofile.pictureUrl ? sessionStorage.setItem("PictureUrl", liffprofile.pictureUrl) : sessionStorage.setItem("PictureUrl", "")
-      await axios
-        .get(`https://www.fastfood.p-e.kr/find_User_Data?User_ID=${encodeURIComponent(liffprofile.userId)}`, {//http://127.0.0.1/service
-        }).then((res) => {
-          if (res.data >= 0){
-            sessionStorage.setItem("User_Point", `${res.data}`)
-            sessionStorage.setItem("On_User", 'On')
-            sessionStorage.setItem("ReCount", 'Y')
-
-          }
-          else {
-            sessionStorage.setItem("User_Point", `0`)
-            sessionStorage.setItem("On_User", 'Off')
-          }
-        })
-    })
-  }
-
   useEffect(() => {
     // alert("index");
     // if (position) {
@@ -56,7 +23,8 @@ const Index: NextPage<{ position: { longitude: number; latitude: number } | null
           router.replace(`/home?${queryString}`, '/')
         }
         else if (!liff.isLoggedIn()){
-          getLiff(liff.login())
+          liff.login()
+          
         }
       }
     // }
